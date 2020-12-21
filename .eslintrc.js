2e5856b70d0b08752c76b0cf1803cc1a5f5203a4
Dong-Ha Kim <dongha.kim210@gmail.com>
2020-12-21 14:41:47 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/ban-ts-comment': 'off',
+  },
+  parser: '@typescript-eslint/parser',
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# kontext common
+
+This is the repository for all common shared JS code of the kontext project.
