31cf037c0af1b97f0c0b215fb31b91bac27214d8
Dong-Ha Kim <dongha.kim210@gmail.com>
2020-12-21 15:15:19 +0100

    Add default ceramic api host

--- src/apis/ceramic.ts
+++ src/apis/ceramic.ts
@@ -1,6 +1,8 @@
 import CeramicClient from '@ceramicnetwork/http-client';
 
-export function createCeramic(ceramicApiHost: string) {
+export function createCeramic(
+  ceramicApiHost: string = 'https://ceramic-dev.3boxlabs.com'
+) {
   const ceramic = new CeramicClient(ceramicApiHost, {
     docSyncEnabled: false,
   });
