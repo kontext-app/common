a84beb58937e22d63b181a4d8e2b560295d72fe5
Dong-Ha Kim <dongha.kim210@gmail.com>
2020-12-21 15:08:39 +0100

    Change default export in index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,22 @@ import * as dotenv from './utils/dotenv';
 import { PUBLISHED_DEFINITIONS } from './constants/definitions';
 import { PUBLISHED_SCHEMAS } from './constants/schemas';
 
-import './types';
-
-export default {
+export const apis = {
   ceramic,
   web3,
+};
+
+export const utils = {
   dotenv,
+};
+
+export const constants = {
   PUBLISHED_DEFINITIONS,
   PUBLISHED_SCHEMAS,
+};
+
+export default {
+  apis,
+  utils,
+  constants,
 };
