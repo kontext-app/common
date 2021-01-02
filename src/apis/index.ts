6625ccbaa670f421ad29c82853e6a1c2cbaf25d9
Dong-Ha Kim <dongha.kim210@gmail.com>
2021-01-02 18:15:36 +0100

    Export ratings api

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -3,3 +3,4 @@ export * as idx from './idx';
 export * as threeId from './threeId';
 export * as profile from './profile';
 export * as bookmarks from './bookmarks';
+export * as ratings from './ratings';
