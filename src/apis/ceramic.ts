047c28c85c0a93bcd47608f94dd558a9d186adca
Dong-Ha Kim <dongha.kim210@gmail.com>
2021-01-23 16:58:02 +0100

    Add ceramic http client config param

--- src/apis/ceramic.ts
+++ src/apis/ceramic.ts
@@ -1,12 +1,15 @@
 import CeramicClient from '@ceramicnetwork/http-client';
 
 import type { CeramicApi } from '@ceramicnetwork/common';
+import type { CeramicClientConfig } from '@ceramicnetwork/http-client';
 
 export function createCeramic(
-  ceramicApiHost: string = 'https://ceramic.kontext.app'
+  ceramicApiHost: string = 'https://ceramic.kontext.app',
+  ceramicConfig: CeramicClientConfig = {}
 ): CeramicApi {
   const ceramic = new CeramicClient(ceramicApiHost, {
     docSyncEnabled: false,
+    ...ceramicConfig,
   });
   return (ceramic as any) as CeramicApi;
 }
