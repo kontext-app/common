bb234e72e4621ae9d338cdc7738568fd68403d32
Dong-Ha Kim <dongha.kim210@gmail.com>
2021-02-08 21:57:25 +0100

    Add api to set all default indices

--- src/apis/idx.ts
+++ src/apis/idx.ts
@@ -1,6 +1,9 @@
 import { IDX } from '@ceramicstudio/idx';
 
 import { aliases } from '../constants';
+import { setDefaultBookmarksIndex } from './bookmarks';
+import { setDefaultListsIndex } from './lists';
+import { setDefaultRatingsIndex } from './ratings';
 
 import type { CeramicApi } from '@ceramicnetwork/common';
 
@@ -10,4 +13,22 @@ export function createIDX(ceramic: CeramicApi) {
     aliases: { ...aliases },
   });
   return idx;
+}
+
+export async function setDefaultKontextIndices() {
+  const [
+    bookmarksIndexDocID,
+    listsIndexDocID,
+    ratingsIndexDocID,
+  ] = await Promise.all([
+    setDefaultBookmarksIndex,
+    setDefaultListsIndex,
+    setDefaultRatingsIndex,
+  ]);
+
+  return {
+    bookmarksIndexDocID,
+    listsIndexDocID,
+    ratingsIndexDocID,
+  };
 }
