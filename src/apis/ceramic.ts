29b481cc6bb3ed5d95efecf3aefe16314b6ad247
Dong-Ha Kim <dongha.kim210@gmail.com>
2021-02-11 10:21:57 +0100

    Fix cermic type

--- src/apis/ceramic.ts
+++ src/apis/ceramic.ts
@@ -1,15 +1,14 @@
 import CeramicClient from '@ceramicnetwork/http-client';
 
-import type { CeramicApi } from '@ceramicnetwork/common';
 import type { CeramicClientConfig } from '@ceramicnetwork/http-client';
 
 export function createCeramic(
   ceramicApiHost: string = 'https://ceramic.kontext.app',
   ceramicConfig: CeramicClientConfig = {}
-): CeramicApi {
+): CeramicClient {
   const ceramic = new CeramicClient(ceramicApiHost, {
     docSyncEnabled: false,
     ...ceramicConfig,
   });
-  return (ceramic as any) as CeramicApi;
+  return ceramic;
 }
